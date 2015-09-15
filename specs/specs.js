5ecfca9a483998913ee00d47bb30a6bcce43c81b
Guest User <student@epicodus.com>
2015-09-15 10:17:44 -0700

    add contact constructor and ticketCost method

--- specs/specs.js
+++ specs/specs.js
@@ -1,27 +1,14 @@
-describe("contact", function() {
+describe("ticket", function() {
   it('creates new contact with given properties', function () {
-    var testContact = new Contact("Andrew", "Dang");
-    expect(testContact.firstName).to.equal("Andrew");
-    expect(testContact.lastName).to.equal("Dang");
-    expect(testContact.addresses).to.eql([]);
+    var testTicket = new Ticket("Ex Machina", 10, 17);
+    expect(testTicket.movieName).to.equal("Ex Machina");
+    expect(testTicket.timeOfDay).to.equal(10);
+    expect(testTicket.age).to.eql(17);
   });
 
-  it("adds the fullName method to all contact", function () {
-    var testContact = new Contact("Michael", "Summers");
-    expect(testContact.fullName()).to.equal("Michael Summers");
-  });
-});
-
-describe('Address', function () {
-  it('creates a new address with the given specs', function() {
-    var testAddress = new Address("123 Main St", "Test City", "Test State");
-    expect(testAddress.street).to.equal("123 Main St");
-    expect(testAddress.city).to.equal("Test City");
-    expect(testAddress.state).to.equal("Test State");
+  it("returns ticket cost based on age and time of day", function() {
+    var testTicket = new Ticket("Ex Machina", 22, 17);
+    expect(testTicket.ticketCost()).to.equal(10);
   });
 
-  it('adds the full address method to all addresses', function() {
-    var testAddress = new Address("123 Main St", "Test City", "Test State");
-    expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
-  });
 });
